4ea618c76e8df63213575929e3a4027cb68540a5
Rana-O <r.000918000@gmail.com>
2020-09-12 10:55:51 +0900

    3rd commit

---
+++ database/migrations/2020_09_08_064901_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->timestamps();
+            $table->string('id');
+            $table->string('photo');
+            $table->string('latLng');
+            $table->string('caption')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
